3d67ec22da9b7aced67f6994b1f5b55393f66871
Zongq <1291342008@qq.com>
2018-11-07 09:31:14 +0800

    提交最新代码

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,25 @@
-console.log('test');
-console.log('hello world');
-console.log('hello');
-console.log('hello');
-console.log('helo');
+// import { resolve } from "dns";
+// import { reject } from "when";
+
+const async = () => {
+    return new Promise((resolve, reject) => {
+        const num = 6;
+        if (num > 5) {
+            resolve(num);
+        } else {
+           reject('num小于5'); 
+        }
+    })
+}
+async()
+    .then((data) => {
+        console.log('first:' + data);
+        return data;
+    })
+    .then((data) => {
+        console.log('second:' + data);
+        return data;
+    })
+    .catch((err) => {
+        console.log(err);
+    })
